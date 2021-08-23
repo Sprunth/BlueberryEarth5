a1754b731f8643abdde6713a3fff49b379a1d9eb
Dylan <sprunth@gmail.com>
2021-08-22 23:04:49 -0700

    initial commit

---
+++ BlueberryEarth5/AppState.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlueberryEarth5
+{
+    public enum ResourceType
+    {
+        BlueberryMush, BlueberryBrick
+    }
+
+    public class AppState
+    {
+        protected Dictionary<ResourceType, long> resources = new();
+
+        public int CounterValue { get; private set; }
+
+        public event Action OnChange;
+
+        private bool Dirty = false;
+        public void MarkDirty() { Dirty = true; }
+
+        public AppState()
+        {
+            foreach (ResourceType t in Enum.GetValues(typeof(ResourceType)))
+            {
+                resources[t] = 0;
+            }
+        }
+
+        public void RedrawIfNeeded()
+        {
+            Console.WriteLine("Drawing!");
+            if (Dirty)
+            {
+                OnChange?.Invoke();
+            }
+            Dirty = false;
+        }
+
+        public void IncrementCounter()
+        {
+            Console.WriteLine("Incrementing!");
+            CounterValue += 1;
+            MarkDirty();
+        }
+
+        public long GetResourceValue(ResourceType rt)
+        {
+            return resources[rt];
+        }
+
+        public void ModifyResourceValue(ResourceType rt, long amount)
+        {
+            Console.WriteLine($"Incrementing {rt.ToString()} by {amount}");
+            resources[rt] += amount;
+            MarkDirty();
+        }
+    }
+}
---
+++ BlueberryEarth5/Services/PeriodicExecutor.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace BlueberryEarth5.Services
+{
+    public class JobExecutedEventArgs : EventArgs { }
+
+    public class PeriodicExecutor : IDisposable
+    {
+        public event EventHandler<JobExecutedEventArgs> JobExecuted;
+        void OnJobExecuted()
+        {
+            JobExecuted?.Invoke(this, new JobExecutedEventArgs());
+        }
+
+        AppState appState;
+        public PeriodicExecutor(AppState appState)
+        {
+            this.appState = appState;
+        }
+
+        Timer _updateTimer, _drawTimer;
+        bool _updateRunning, _drawRunning;
+
+        public void StartExecuting()
+        {
+            if (!_updateRunning)
+            {
+                // Initiate a Timer
+                _updateTimer = new Timer();
+                _updateTimer.Interval = 1000;  // ms
+                _updateTimer.Elapsed += OnUpdateTimerTrigger;
+                _updateTimer.AutoReset = true;
+                _updateTimer.Enabled = true;
+
+                _updateRunning = true;
+            }
+            if (!_drawRunning)
+            {
+                // Initiate a Timer
+                _drawTimer = new Timer();
+                _drawTimer.Interval = 5000;  // ms
+                _drawTimer.Elapsed += OnDrawTimerTrigger;
+                _drawTimer.AutoReset = true;
+                _drawTimer.Enabled = true;
+
+                _drawRunning = true;
+            }
+        }
+        void OnUpdateTimerTrigger(object source, ElapsedEventArgs e)
+        {
+            // Execute required job
+            appState.IncrementCounter();
+            // Notify any subscribers to the event
+            OnJobExecuted();
+        }
+
+        void OnDrawTimerTrigger(object source, ElapsedEventArgs e)
+        {
+            appState.RedrawIfNeeded();
+            // call draw subscribers event if needed?
+        }
+
+        public void Dispose()
+        {
+            if (_updateRunning)
+            {
+                // Clear up the timer
+                _updateTimer.Dispose();
+            }
+            if (_drawRunning)
+            {
+                _drawTimer.Dispose();
+            }
+        }
+    }
+}
